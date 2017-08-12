24acb7d14dac914dfaf266f68508bbaa457b158a
digi <james@digijin.com>
2017-08-12 23:53:01 +1000

    deploying

--- deploy.js
+++ deploy.js
@@ -2,11 +2,12 @@
 
 var Application = require('beanstalkify');
 
+var secret = require('./secret.json');
 
 var application = new Application(
     {
-        accessKeyId: 'XXX',
-        secretAccessKey: 'XXX',
+        accessKeyId: secret.accessKeyId,
+        secretAccessKey: secret.secretAccessKey,
         region: 'ap-southeast-2'
     }
 );
@@ -14,17 +15,17 @@ var application = new Application(
 // Deploy
 application.deploy(
 {
-    archiveFilePath: 'PATH TO ZIP FILE',
-    environmentName: 'CNAME',
-    awsStackName: '64bit Amazon Linux 2015.03 v2.0.0 running Node.js',
+    archiveFilePath: 'package-v1.zip',
+    environmentName: 'mypetcname',
+    awsStackName: '64bit Amazon Linux 2015.03 v2.0.0 running Node.js',//64bit Amazon Linux 2017.03 v2.7.1 running Multi-container Docker 17.03.1-ce 
     beanstalkConfig: [
-        Beanstalk options
-        ....
     ]
 }
 ).then(function(data){
-    console.log(data); # {app_name: 'test-website', app_version: 'foo', env_name: 'test-website-prod', env_url: 'tech-website-12345.ap-southeast-2.elasticbeanstalk.com'}
+    console.log(data); // {app_name: 'test-website', app_version: 'foo', env_name: 'test-website-prod', env_url: 'tech-website-12345.ap-southeast-2.elasticbeanstalk.com'}
+}, function(reason){
+    console.warn(reason);
 });
 
 // Clean application versions
-application.cleanApplicationVersions('application name'); // Returns a promise
+// application.cleanApplicationVersions('application name'); // Returns a promise
